7187ce87c00477bc78e5b8eccc30fc88b936fa82
Peter <pcifarelli21@gmail.com>
2020-06-30 16:03:50 -0500

    edit todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
+import NewTodoForm from './NewTodoForm' 
 import Todo from './Todo';
 
 class TodoList extends Component {
     constructor(props) {
         super(props);
-        this.state = { todos: [{task: "Walk the Fish"}, {task: "Groom Chickens"}] };
+        this.state = { todos: [] 
+    };
+    this.create = this.create.bind(this);
+    this.remove = this.remove.bind(this);
+    this.update = this.update.bind(this);
+    this.toggleCompletion = this.toggleCompletion.bind(this);
+    };
+   
+    create(newTodo) {
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        })
+    }
+    remove(id){
+        this.setState({
+            todos: this.state.todos.filter(t => t.id !== id)
+        })
+    }
+    update(id, updatedTask){
+        const updateTodos = this.state.todos.map(todo => {
+            if(todo.id === id){
+                return {...todo, task: updatedTask}
+            }
+            return todo;
+        });
+        this.setState({todos: updateTodos})
+    }
+    toggleCompletion(id){
+        const updateTodos = this.state.todos.map(todo => {
+            if(todo.id === id){
+                return {...todo, completed: !todo.completed}
+            }
+            return todo;
+        });
+        this.setState({todos: updateTodos})
     }
     render() {
         const todos = this.state.todos.map(todo => {
-            return <Todo task={todo.task} />;
+            return <Todo 
+            key={todo.id} 
+            id={todo.id} 
+            task={todo.task} 
+            completed={todo.completed}
+            removeTodo={this.remove} 
+            updateTodo={this.update}
+            toggleTodo={this.toggleCompletion}
+            />;
         });
         return (
             <div>
                 <h1>Todo List!</h1>
+                <NewTodoForm createTodo={this.create} />
                 <ul>
                     {todos}
                 </ul>
